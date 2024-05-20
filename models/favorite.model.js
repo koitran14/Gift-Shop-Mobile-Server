b7be22dc349dca293014d02e88e503bea51c8d40
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2024-05-20 23:37:36 +0700

    Fixing Favorite

--- models/favorite.model.js
+++ models/favorite.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose'); // Erase if already required
 const User = require('./user.model');
 const Product = require('./product.model');
-const Shop = require('./shop.model');
+const Store = require('./store.model');
 
 var favoriteSchema = new mongoose.Schema({
     user: User.schema,
     product: [Product.schema],
-    shop: [Shop.schema]
+    store: [Store.schema]
 });
 
 module.exports = mongoose.model('Favorite', favoriteSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -16,9 +16,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    token: {
-        type: String
-    }
+    // token: {
+    //     type: String
+    // }
 });
 
 module.exports = mongoose.model("User", userSchema);
