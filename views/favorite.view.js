7a26c1c55e8c3eb89e5633037ed82eb1c640f262
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2024-05-21 20:27:51 +0700

    Update favorite.view.js

--- views/favorite.view.js
+++ views/favorite.view.js
@@ -2,11 +2,8 @@ const { getAll, getFavoriteForEachUser, createFavorite } = require('../controlle
 
 module.exports = function (app) {
     app.get('/favorite', getAll);
-}
-module.exports = function (app) {
     app.get('/favorite/user', getFavoriteForEachUser);
-}
-module.exports = function (app) {
     app.post('/favorite/create', createFavorite);
 }
+
 
